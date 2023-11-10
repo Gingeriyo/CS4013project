f28f6663cfc5e0b6f811082442dd258a85d0d382
Eric1641 <ericlonerganat@gmail.com>
2023-11-10 02:22:41 +0000

    I added a test implementation
    you can delete if you want i just thought it might be nice to start thinking about

--- src/main/Semester.java
+++ src/main/Semester.java
@@ -5,3 +5,55 @@ public class Semester {
     // The data from the programme the student os doing determines
     // how the QCA is calculated.
 }
+//here is a test implement for QCA calculator from eric i can explain it when i see yall but
+//this might be a nice start to build upon
+//import java.util.HashMap;
+//import java.util.*;
+//
+//public class Semester {
+//
+//    HashMap<String, Double> Grade = new HashMap<>();
+//
+//
+//    {
+//
+//        Grade.put("A1",4.0);
+//        Grade.put("A2",3.6);
+//        Grade.put("B1",3.2);
+//        Grade.put("B2",3.0);
+//        Grade.put("B3",2.8);
+//        Grade.put("C1",2.60);
+//        Grade.put("C2",2.4);
+//        Grade.put("C3",2.0);
+//        Grade.put("D1",1.6);
+//        Grade.put("D2",1.2);
+//        Grade.put("F",0.0);
+//        Grade.put("NG",0.0);
+//
+//
+//    }
+//    public double getGrade(String Name) {
+//        return this.Grade.get(Name);
+//    }
+//    public double QCACalc(Module credits,String Grade, int sumAttended, int nonQ) {
+//        //very basic implementation
+//        double s = getGrade(Grade);
+//        double QCA = (getGrade(Grade) * credits.getCredits())/ (sumAttended- nonQ);
+//        return QCA;
+//
+//    }
+//    public class Module {
+//        String name;
+//        String classCode;
+//        int credits;
+//
+//        public Module(String name , String classCode, int credits) {
+//            this.name = name;
+//            this.classCode = classCode;
+//            this.credits= credits;
+//        }
+//
+//        public int getCredits() {
+//            return credits;
+//        }
+//    }
