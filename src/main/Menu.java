219a14ff984eb0372c33c09fd5413c491e86a5d3
MarkBarry24 <22299289@studentmail.ul.ie>
2023-11-14 23:08:06 +0000

    moved code from user.java to menu,java. + notes

--- src/main/Menu.java
+++ src/main/Menu.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Menu {
     public static void main(String[] args) {
@@ -10,13 +11,61 @@ public class Menu {
         // It will print out all the data to produce a transcript.
 
         /*
-        testing the student transcript
-
-        Student me = new Student();
-
-        System.out.println(me.transcript());
-
+         * testing the student transcript
+         * 
+         * Student me = new Student();
+         * 
+         * System.out.println(me.transcript());
+         * 
          */
 
+        /*
+         * this is the first thing that pops up when the project is run
+         * it creates a scanner and asks if you are logging in as a student or faculty
+         * member
+         * if a student is logging in then it asks for an id number.
+         * If faculty is logging in it asks for first and last names and checks through
+         * a csv file of faculty to grant access
+         * if anyone has a better check for this feel free to change it cuz there is a
+         * fault where a student can just enter
+         * a lecturers name for access
+         *
+         *
+         * NOTE FOR MYSELF: when an id number is put in the system should scan through a
+         * csv file of students
+         * and it pull the names and addresses from there. the userstudent class should
+         * hold a method for this scan
+         * UserStudent should grant the permissions to change personal details like
+         * address etc.
+         */
+        final String firstName;
+        final String lastName;
+        final int idNumber;
+        final Scanner in;
+
+        try {
+            in = new Scanner(System.in);
+            boolean more = true;
+
+            while (more) {
+                System.out.println("S)tudent or F)aculty");
+                String command = in.nextLine().toUpperCase();
+
+                if (command.equals("S")) {
+                    System.out.println("Enter StudentID number");
+                    String line = in.nextLine();
+
+                } else if (command.equals("F")) {
+                    System.out.println("Enter First name and Last name");
+                    String nameLine = in.nextLine();
+
+                }
+
+            }
+
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
     }
+
 }
--- src/main/User.java
+++ src/main/User.java
@@ -9,35 +9,4 @@ public class User {
     // A user is used alongside execution to determine what permissions
     // they have.
 
-    private String firstName;
-    private String lastName;
-    private int idNumber;
-    private Scanner in;
-
-    public studentLogin(String firstName, String lastName, int idNumber) {
-        try {
-            in = new Scanner(System.in);
-            boolean more = true;
-
-            while (more) {
-                System.out.println("S)tudent or F)aculty");
-                String command = in.nextLine().toUpperCase();
-
-                if (command.equals("S")) {
-                System.out.println("Enter StudentID number");
-                String line = in.nextLine();
-
-            } else if (command.equals("F")) {
-                System.out.println("Enter First name and Last name");
-                String nameLine = in.nextLine();
-
-            }
-
-            }
-
-
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
-    }
 }
