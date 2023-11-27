837b5000b7fd821059850c8a95d9c65b36fa8c2d
leo <22342761@studentmail.ul.ie>
2023-11-27 16:10:56 +0000

    Main Menu and New Scenes
    JavaFX

--- src/main/Menu.java
+++ src/main/Menu.java
@@ -1,7 +1,12 @@
 package main;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -14,6 +19,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TableColumn;
 
 // Gridpane
 // https://docs.oracle.com/javase/8/javafx/api/javafx/scene/layout/GridPane.html?external_link=true
@@ -21,12 +28,14 @@ import javafx.scene.control.TextField;
 public class Menu extends Application {
 
     private Stage stage;
+    private int minWidth = 720;
+    private int minHeight = 480;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-
         stage = primaryStage;
         primaryStage.setTitle("UL Portal");
+        primaryStage.setResizable(false);
         primaryStage.setScene(login());
         primaryStage.show();
     }
@@ -42,10 +51,10 @@ public class Menu extends Application {
 
         header.setFont(Font.font("Verdana", FontWeight.EXTRA_BOLD, 20)); 
         idLabel.setFont(Font.font("Verdana", FontWeight.BOLD, 10)); 
-        pwLabel.setFont(Font.font("Verdana", FontWeight.EXTRA_BOLD, 10)); 
-        loginButton.setOnAction(e -> stage.setScene(homeMenu()));
+        pwLabel.setFont(Font.font("Verdana", FontWeight.BOLD, 10)); 
         idLabel.setTextFill(Color.web("#FF0000"));
         pwLabel.setTextFill(Color.web("#FF0000"));
+        loginButton.setOnAction(e -> stage.setScene(homeMenu()));
         
         VBox p1 =   new VBox(
                     header,
@@ -61,21 +70,189 @@ public class Menu extends Application {
         p1.setMinSize(600, 500);
         p1.setStyle("-fx-padding: 180;");
 
-        return new Scene(p1, 720, 640);
+        return new Scene(p1, minWidth, minHeight);
     }
-
 
     private Scene homeMenu(){
         GridPane layout = new GridPane();
 
-        // the idea for a sign out button is to allow the user to log out and sign in as someone else
-        // this would probably need to restart the program?
-        Button signOut = new Button();
+        Label header = new Label("Home");
 
-        
+        header.setFont(Font.font("Verdana", FontWeight.EXTRA_BOLD, 20)); 
 
-        return new Scene(layout);
+        layout.add(header, 0, 1); // col, row   
+        layout.add(myResults(), 0, 2);
+        layout.add(myModules(), 0, 3);
+        layout.add(options(), 1, 3);
+
+        layout.setStyle("-fx-padding: 20;");
+        layout.getColumnConstraints().add(new ColumnConstraints((minWidth/2) - 50)); 
+        layout.getColumnConstraints().add(new ColumnConstraints((minWidth/2) - 50)); 
+        layout.setVgap(20);
+        layout.setHgap(20);
+
+        return new Scene(layout, minWidth, minHeight);
     }
+
+    private VBox myResults(){
+        Label header = new Label("My Results");
+        Button currentResults = new Button("Current Results");
+        Button transcript = new Button("Student Transcript");
+
+        VBox layout =   new VBox(header,
+                        currentResults,
+                        transcript);
+
+        header.setFont(Font.font("Verdana", FontWeight.BOLD, 10)); 
+        layout.setAlignment(Pos.CENTER_LEFT);
+        layout.setSpacing(6);
+        layout.setMinSize(100, 100);
+        layout.setStyle("-fx-padding: 10;");
+        currentResults.setMinWidth(200);
+        transcript.setMinWidth(200);
+        currentResults.setOnAction(e -> stage.setScene(currentResults()));
+        transcript.setOnAction(e -> stage.setScene(studentTranscript()));
+
+        return layout;
+    }
+
+    private VBox myModules(){
+        Label header = new Label("Current Modules");
+        TableView<Module> table = new TableView<Module>();
+
+        /* 
+        should also include the below two rows but since im using the 'module' class which doesnt include these I deleted them
+        TableColumn<Module, String> yearCol = new TableColumn<Module, String>("Year");
+        TableColumn<Module, String> semCol = new TableColumn<Module, String>("Period");
+        */
+        TableColumn<Module, String> modCol = new TableColumn<Module, String>("Module");
+        TableColumn<Module, String> credCol = new TableColumn<Module, String>("Credits");
+        TableColumn<Module, String> nameCol = new TableColumn<Module, String>("Name");
+
+        // this works but there is empty space in the table
+        // and when I include cols for year and period it's too many cols
+        table.getColumns().addAll(modCol, credCol, nameCol);
+
+        // why does this not display in the table
+        table.getItems().add(new Module("name", 6, "CS4441"));
+
+        VBox layout = new VBox(header, table);
+        layout.setAlignment(Pos.CENTER_LEFT);
+        layout.setSpacing(6);
+        layout.setMinSize(100, 100);
+        layout.setStyle("-fx-padding: 10;");
+        header.setFont(Font.font("Verdana", FontWeight.BOLD, 10)); 
+
+        return layout;
+    }
+
+    private VBox options(){
+        Label header = new Label("Options");
+        Button pwChange = new Button("Change Password");
+        Button details = new Button("View/Edit Personal Details");
+        Button emailChange = new Button("Update Email Address");
+        Button logout = new Button("Logout");
+
+        VBox layout =   new VBox(header,
+                        pwChange,
+                        details,
+                        emailChange,
+                        logout);
+
+        layout.setAlignment(Pos.CENTER_LEFT);
+        layout.setSpacing(6);
+        layout.setMinSize(100, 100);
+        layout.setStyle("-fx-padding: 10;");
+        header.setFont(Font.font("Verdana", FontWeight.BOLD, 10)); 
+        pwChange.setMinWidth(200);
+        details.setMinWidth(200);
+        emailChange.setMinWidth(200);
+        logout.setMinWidth(200);
+
+        pwChange.setOnAction(e -> stage.setScene(changePassword()));
+        details.setOnAction(e -> stage.setScene(personalDetails()));
+        emailChange.setOnAction(e -> stage.setScene(updateEmail()));
+        logout.setOnAction(e -> stage.setScene(logout()));
+
+        return layout;
+    }
+
+    private Scene currentResults(){
+
+
+        return new Scene(null);
+    }
+
+    private Scene studentTranscript(){
+
+
+        return new Scene(null);
+    }
+    
+    private Scene changePassword(){
+        Button home = new Button("Home");
+        home.setOnAction(e -> stage.setScene(homeMenu()));
+        Button go = new Button("Go");
+        // this should be changed to actually change the password
+        go.setOnAction(e -> stage.setScene(homeMenu()));
+
+        Label header = new Label("Change Password");
+        header.setFont(Font.font("Verdana", FontWeight.EXTRA_BOLD, 20)); 
+
+        Label old = new Label("Old Password");
+        Label newPlabel = new Label("New Password");
+        Label confirm = new Label("Confirm New Password");
+        old.setFont(Font.font("Verdana", FontWeight.BOLD, 10)); 
+        newPlabel.setFont(Font.font("Verdana", FontWeight.BOLD, 10)); 
+        confirm.setFont(Font.font("Verdana", FontWeight.BOLD, 10)); 
+        old.setTextFill(Color.web("#FF0000"));
+        newPlabel.setTextFill(Color.web("#FF0000"));
+        confirm.setTextFill(Color.web("#FF0000"));
+
+        PasswordField oldpw = new PasswordField();
+        PasswordField newpw = new PasswordField();
+        PasswordField confirmpw = new PasswordField();
+
+        VBox layout = new VBox(
+                                home,
+                                new Label(""), // this is here to act as a blank space
+                                header,
+                                old, 
+                                oldpw, 
+                                newPlabel,
+                                newpw,
+                                confirm,
+                                confirmpw,
+                                go);
+
+        layout.setAlignment(Pos.CENTER_LEFT);
+        layout.setSpacing(6);
+        layout.setMinSize(600, 500);
+        layout.setStyle("-fx-padding: 180;");
+
+        return new Scene(layout, minWidth, minHeight);
+    }
+
+    private Scene personalDetails(){
+
+
+        return new Scene(null);
+    }
+
+    private Scene updateEmail(){
+
+
+        return new Scene(null);
+    }
+    
+    // the idea for a sign out button is to allow the user to log out and sign in as someone else
+    // this would probably need to restart the program?
+    // also this probably does not need to be a scene
+    private Scene logout(){
+        return new Scene(null);
+    }
+
+    
 
     public static void main(String[] args) {
         launch();
