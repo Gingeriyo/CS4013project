be5917fe7ebd2f9dee8b0a2d8b862fc821f99a8f
Eric1641 <ericlonerganat@gmail.com>
2023-12-01 01:00:53 +0000

    for leo's menu(not fully done yet)
    this adds the method leo requested its not fully done yet as i still need to test if it works with different semesters and there is no bugs

--- src/main/Module.java
+++ src/main/Module.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Module {
     private String name;
@@ -48,5 +49,47 @@ public class Module {
 
     public int[] getMarkingScheme() {
         return markingScheme;
+    }
+
+    public static String [] Transciptinfo(String id) throws FileNotFoundException{
+        String [] mod = new  String [3];
+        Scanner s = new Scanner(new File("src/csv/facultyLogin.csv"));
+        s.useDelimiter(",");
+        String temp;
+        while(s.hasNext()){
+            temp = s.next();
+            temp = temp.replaceAll("\n","");
+            if(temp.equals(id)) {
+                mod[0] = id;//modcode
+                mod[2] = s.next();//mod name
+                mod[1] = s.next();//mod credits
+                break;
+            }
+
+        }
+        return mod;
+    }
+//returns 3d array with the specified information once you input the users id
+//the info returned is the code for the modules ,credits and name for all 
+//the modules the user does
+//right now it isint done as the semester is hard coded to be 0 so i might make it so 
+// you have to specify the semester as well
+    public static String [][] leo (int id,int semester)throws FileNotFoundException {
+        String [][] sab = new String[100][100];
+        ArrayList<Module> sm = new ArrayList<>();
+        StudentGrades grade = new StudentGrades(id, "src/csv/students.csv", "src/csv/grades.csv");
+        String coursecode =grade.getCourseCode(semester);
+        Semester sem = new Semester(30, coursecode, semester, "src/csv/course.csv", "src/csv/modules.csv");
+        sm = sem.getModules();
+        for(int i = 0;i<sm.size();i++) {
+            int credits =sm.get(i).getCredit();
+            String name =sm.get(i).getName();
+            String code = sm.get(i).getClassCode();
+            sab[i][0] = code;
+            sab[i][1] = credits+"";
+            sab[i][2] = name;
+
+        }
+        return sab;
     }
 }
